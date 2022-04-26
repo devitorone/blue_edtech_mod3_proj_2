22e177ae72b3f3894ebd85640dee42ff4083df27
devitorone <vitor@cazebre.com>
2022-04-26 17:07:25 -0300

    test: testing heroku mongo

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const port = 3000; 
 const app = express();
 const cors = require('cors');
-const route = require('./src/routes/char.route');
+const route = require('./src/routes/char.route')
+const connectToDatabase = require('./src/database/database');
 
 app.use(express.json());
 app.use(cors());
